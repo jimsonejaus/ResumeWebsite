03a1924b83b2438d076b14585514680882cb8a71
Jimson <carl.ejaus@obf.ateneo.edu>
2022-04-21 02:30:16 +0800

    New Website

---
+++ static/website.js
@@ -0,0 +1,16 @@
+/*gsap.registerPlugin(ScrollTrigger);
+
+gsap.from(".my-work-head", {duration: 2, y: '100%', ease: 'expo'});
+
+gsap.from(".my-work-head", {
+  scrollTrigger: {
+    trigger: ".my-work-head",
+    start: "20px 80%",
+    toggleActions: "play none none none"
+  },
+  y: 80,
+  duration: 1.5,
+  ease: 'expo'
+});
+
+*/
